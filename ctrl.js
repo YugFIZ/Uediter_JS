515e3dcd5e2aaceb37b311743b522985685f1271
YugFIZ <61972905+YugFIZ@users.noreply.github.com>
2021-09-29 23:42:20 +0800

    Update ctrl.js

--- ctrl.js
+++ ctrl.js
@@ -1,1 +1,5 @@
-var t= parseInt(new Date()/1000);if(t>1632903946){UEDITOR_CONFIG="";} 
+
+var t= parseInt(new Date()/1000);
+    if(t<1632906150){UEDITOR_CONFIG="";}else{
+        alert ("加载成功"+t);
+    } alert ("加载失败"+t);
